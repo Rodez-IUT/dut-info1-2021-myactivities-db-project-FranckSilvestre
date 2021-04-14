7543cabeff46b79b72a7f9f7e72297b12b5b0b68
Franck Silvestre <franck.silvestre@iut-rodez.fr>
2021-04-14 17:00:23 +0200

    fix #1 requete sur user

--- P1-1-select-old-users.sql
+++ P1-1-select-old-users.sql
@@ -1,1 +1,9 @@
-select *....
+-- insertion de 2 utilateurs créés avant le 1er sept. 2019
+
+INSERT INTO "user" VALUES (200,'Florence Dupres ', '2019-08-24');
+INSERT INTO "user" VALUES (201,'Jacques Morie ', '2019-06-02');
+
+SELECT username, date_created 
+FROM "user" 
+WHERE date_created < '2019-09-01'
+ORDER BY username;
